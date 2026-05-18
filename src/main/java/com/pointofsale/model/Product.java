5d7218cbb25e3408776b9f66640a286699f2f2af
Pedro Guilherme <pedropkr3@gmail.com>
2026-05-18 20:57:21 -0300

    [Product] - Atributos e Getters

--- src/main/java/com/pointofsale/model/Product.java
+++ src/main/java/com/pointofsale/model/Product.java
@@ -1,4 +1,24 @@
 package com.pointofsale.model;
 
 public class Product {
+    private String name;
+    private int value;
+    private int id;
+    private String linkImage;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLinkImage() {
+        return linkImage;
+    }
 }
