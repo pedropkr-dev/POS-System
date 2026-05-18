bea5f2f136506a631a9922589db109fd1d9d7bef
Pedro Guilherme <pedropkr3@gmail.com>
2026-05-18 20:37:23 -0300

    Estrutura das pastas

---
+++ src/main/java/com/pointofsale/Main.java
@@ -0,0 +1,9 @@
+package com.pointofsale;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        }
+    }
+}
---
+++ src/main/java/com/pointofsale/model/Produto.java
@@ -0,0 +1,4 @@
+package com.pointofsale.model;
+
+public class Produto {
+}
