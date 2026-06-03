63a831e4d3ca9f323d188349b454b8474dfb7667
Pedro Guilherme <pedropkr3@gmail.com>
2026-06-02 21:38:31 -0300

    [ProdutoVenda][Atualizado] - Atribuitos, Construtores, BigDecimal  Subtotal, get e set, override

--- src/main/java/com/pointofsale/model/ProdutoVenda.java
+++ src/main/java/com/pointofsale/model/ProdutoVenda.java
@@ -1,13 +1,65 @@
 package com.pointofsale.model;
 
-public class ProdutoVenda {
-    private Produto produto;
-    private short quantidade;
-    private float valor;
+import java.math.BigDecimal;
 
-    public ProdutoVenda(Produto produto, short quantidade, float valor) {
-        this.produto = produto;
+public class ProdutoVenda {
+    private String codigoBarras;
+    private String nome;
+    private int quantidade;
+    private BigDecimal valorUnitario;
+
+    public ProdutoVenda(){
+
+    }
+
+    public ProdutoVenda(Produto produto, int quantidade) {
+        this.codigoBarras = produto.getCodigoBarras();
+        this.nome = produto.getNome();
         this.quantidade = quantidade;
-        this.valor = valor;
+        this.valorUnitario = produto.getValor();
+    }
+
+    public BigDecimal getSubtotal(){
+        if (valorUnitario == null) {
+            return BigDecimal.ZERO;
+        }
+        return valorUnitario.multiply(BigDecimal.valueOf(quantidade));
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(BigDecimal valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    @Override
+    public String toString() {
+        return quantidade + "x " + nome + " @ " + valorUnitario;
     }
 }
